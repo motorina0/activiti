af392442b6a34f7d4850040175ff63064c5c4c6b
Vlad Stan <stan.v.vlad@gmail.com>
2016-10-25 18:11:29 +0300

    initial commit

---
+++ samples/ten-minute-demo/src/main/java/org/activiti/demo/App.java
@@ -0,0 +1,24 @@
+package org.activiti.demo;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		//ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+		ProcessEngine engine =  ProcessEngineConfiguration
+				.createProcessEngineConfigurationFromResource("org/activiti/demo/activiti.cfg.xml").buildProcessEngine();
+		
+		
+		RepositoryService repositoryService = engine.getRepositoryService();
+
+		Deployment deployment = repositoryService.createDeployment()
+				.addClasspathResource("org/activiti/demo/FinancialReportProcess.bpmn20.xml").deploy();
+	}
+}
